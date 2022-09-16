0aa9400fb7d83037623d07cb393b77000e0e41ce
sukivn <ndbao202@clc.fitus.edu.vn>
2022-09-16 20:46:37 +0700

    age

--- src/test/main.py
+++ src/test/main.py
@@ -1,13 +1,19 @@
 class Main:
-    def __init__(self, name):
+    def __init__(self, name, age):
         self.name = name
+        self.age = age
 
     def print(self):
         print('this is test project, welcome ', self.name);
+        
+    def getAge(self):
+        return self.age
 
 def main():
-    obj = Main('DS')
+    obj = Main('DS', 19)
     obj.print()
+    
+    print(obj.getAge())
 
 if __name__ == "__main__":
     main()
