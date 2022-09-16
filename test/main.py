d55eda1719bc94372f38f78aabe69b28bca3847f
Albert <73238186+quytrungg@users.noreply.github.com>
2022-09-16 20:22:46 +0700

    initial commit

---
+++ test/main.py
@@ -0,0 +1,16 @@
+from copyreg import constructor
+
+
+class Main:
+    def __init__(self, name):
+        self.name = name
+
+    def print(self):
+        print('this is test project, welcome ', self.name);
+
+def main():
+    obj = Main('DS')
+    obj.print()
+
+if __name__ == "__main__":
+    main()
