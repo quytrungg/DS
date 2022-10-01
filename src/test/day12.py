9d9d1b954c80614bcf7e8231fa73799a0df7ae97
Albert <73238186+quytrungg@users.noreply.github.com>
2022-10-01 11:00:38 +0700

    initialize day 12 -> 15

---
+++ src/test/day12.py
@@ -0,0 +1,25 @@
+def intersection(a, b):
+
+    return 
+
+class Node(object):
+  def __init__(self, val):
+    self.val = val
+    self.next = None
+  def prettyPrint(self):
+    c = self
+    while c:
+      print(c.val)
+      c = c.next
+
+a = Node(1)
+a.next = Node(2)
+a.next.next = Node(3)
+a.next.next.next = Node(4)
+
+b = Node(6)
+b.next = a.next.next
+
+c = intersection(a, b)
+c.prettyPrint()
+# 3 4
---
+++ src/test/day13.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def pushDominoes(self, dominoes):
+        return
+
+print(Solution().pushDominoes('..R...L..R.'))
+# ..RR.LL..RR
