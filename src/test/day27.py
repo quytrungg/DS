6372c0eaa9a78fe6891f6306834b9ee2e6d1c747
Albert <73238186+quytrungg@users.noreply.github.com>
2022-10-15 08:01:26 +0700

    update course folder

---
+++ src/test/day27.py
@@ -0,0 +1,8 @@
+class Solution:
+  def buddyStrings(self, A, B):
+    return
+
+print(Solution().buddyStrings('aaaaaaabc', 'aaaaaaacb'))
+# True
+print(Solution().buddyStrings('aaaaaabbc', 'aaaaaaacb'))
+# False
