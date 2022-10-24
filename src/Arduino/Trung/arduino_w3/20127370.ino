50b67a28e6fee8f40ce55ff9affbe624da82b263
Albert <73238186+quytrungg@users.noreply.github.com>
2022-10-25 00:55:46 +0700

    lab 0 and lab 1 ds code + modify arduino

--- src/Arduino/Trung/arduino_w3/20127370.ino
+++ src/Arduino/Trung/arduino_w3/20127370.ino
@@ -12,24 +12,6 @@ void setup()
   Serial.begin(9600);
 }
 
-// Function to check if LED is HIGH and button is LOW,
-// the LED will turn off right away
-void checkLed(){
-  	for(int i = 0; i < 10; ++i){
-  		if(!digitalRead(button)){
-  			return;
-    	}
-      	delay(100);
-    }
-}
-
-// Function to turn a LED on for 1s and then off
-void turnOnLed(int led){
-	digitalWrite(led, HIGH);
-  	checkLed();
-    digitalWrite(led, LOW);
-}
-
 // Function to turn on all of the LEDs
 void turnOnAllLed(int* arr, int n){
 	for(int i = 0; i < n; ++i){
@@ -56,12 +38,18 @@ void loop()
   
   if (buttonState) { // Button is clicked
     int start = millis(), idx = 0;
+    int check = millis();
     while(digitalRead(button)){ // Button is clicked and held
-      	if(millis() - start <= 12000){ // Duration <= 12s
-      		turnOnLed(led[idx]);
-          	idx = (idx + 1) % numOfLed;
+      	if(millis() - start > 12000)
+          	modifyAllLed(led, numOfLed, turnOnAllLed);
+        else{ // Duration <= 12s
+      		digitalWrite(led[idx], HIGH);
+          	if(millis() - check >= 1000){ // After 1s change LED
+              	digitalWrite(led[idx], LOW);
+              	idx = (idx + 1) % numOfLed;
+          		check = millis();	
+            }
       	}
-      	else modifyAllLed(led, numOfLed, turnOnAllLed);
     }
   }	
   modifyAllLed(led, numOfLed, turnOffAllLed);
