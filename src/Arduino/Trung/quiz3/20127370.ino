b3e23042e5151130de4baaf48fd7098df0aa2859
Albert <73238186+quytrungg@users.noreply.github.com>
2022-11-05 08:45:50 +0700

    update quiz3 arduino, day 44,45,46,47 interview, w4 dscode, w4 ML

--- src/Arduino/Trung/quiz3/20127370.ino
+++ src/Arduino/Trung/quiz3/20127370.ino
@@ -8,7 +8,7 @@ void setup()
 {
   pinMode(port, INPUT);
   lcd_1.begin(16, 2); // Set up the number of columns and rows on the LCD
-  lcd_1.print("hello world!"); // LCD initial print
+  lcd_1.print("Temperature is"); // LCD initial print
   Serial.begin(9600);
 }
 
@@ -18,4 +18,6 @@ void loop()
   float t = ((tmp_value*5 / 1023) / 0.01) - 50; // convert analog value to temp
   lcd_1.setCursor(0, 1); // set cursor to correct position
   lcd_1.print(t);
+  lcd_1.setCursor(6, 1);
+  lcd_1.print("\'C");
 }
--- src/interview/day41.py
+++ src/interview/day41.py
@@ -1,7 +1,11 @@
 import collections
 
 def groupAnagramWords(strs):
-    
+    if not strs:
+        return
+    idx = 0
+    result = []
+    result.append([strs[0]])
     return
 
 print(groupAnagramWords(['abc', 'bcd', 'cba', 'cbd', 'efg']))
